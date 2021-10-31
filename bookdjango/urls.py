ef49e6451ae4ae5b96ee0a1cd1cb66c6f57179d8
Daniel Correa <yo@danielgara.com>
2021-10-31 10:41:43 -0500

    imp: chapter 10

--- bookdjango/urls.py
+++ bookdjango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from movie import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +26,5 @@ urlpatterns = [
     path('about/', views.about),
     path('signup/', views.signup, name='signup'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
